fc0c9a21ea79cecdb87e84e4309d7b445cacbd4d
Prisciliano Cortés <pris98.cortes@gmail.com>
2023-10-07 23:27:44 -0600

    fix: Update test in elements with vite

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,23 +1,25 @@
 import { useState } from 'react';
 
-export const useForm = ( initialState = {} ) => {
-    
-    const [values, setValues] = useState(initialState);
+export const useForm = ( initialForm = {} ) => {
+  
+    const [ formState, setFormState ] = useState( initialForm );
 
-    const reset = () => {
-        setValues( initialState );
-    }
-
-
-    const handleInputChange = ({ target }) => {
-
-        setValues({
-            ...values,
-            [ target.name ]: target.value
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [ name ]: value
         });
-
     }
 
-    return [ values, handleInputChange, reset ];
+    const onResetForm = () => {
+        setFormState( initialForm );
+    }
 
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        onResetForm,
+    }
 }
--- src/types/types.js
+++
@@ -1,5 +0,0 @@
-
-export const types = {
-    login : '[auth] Login',
-    logout: '[auth] Logout'
-}
